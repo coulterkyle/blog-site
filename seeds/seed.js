cc8fcdeb3325d14864ebc7dd7715964a6ba32288
kyle coulter <152663440+coulterkyle@users.noreply.github.com>
2024-02-08 10:59:01 -0500

    added seed data

---
+++ seeds/seed.js
@@ -0,0 +1,33 @@
+const sequelize = require('../config/connection');
+const { User, Blogpost, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blogpost of blogData) {
+    await Blogpost.create({
+      ...blogpost,
+    //   user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+    //   user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
